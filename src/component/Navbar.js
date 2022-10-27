b86f6e043850674b78db1c74111e583e75e3c314
liams <princewill835@gmail.com>
2022-10-26 19:14:15 -0700

    creating navbar

---
+++ src/component/Navbar.js
@@ -0,0 +1,16 @@
+const Nav = () => {
+    return(
+        <nav>
+            <div className="intl">
+                <h6>current score:</h6>
+                <h6>best score:</h6>
+            </div>
+        </nav>
+    )
+}
+// use sass for stying
+// add a toggle button from light to dark
+// goodluck on remebering this.
+// look up api pictures for cars or anime
+// Look up card designs in the odin project
+export default Nav;
